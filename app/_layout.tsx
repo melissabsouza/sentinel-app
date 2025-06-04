6d49bcda3ade5e4efe77d247e7336cca00fdc59f
Melissa <melissabsouzaa@gmail.com>
2025-06-04 16:15:19 -0300

    feat: tailwind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,21 +5,8 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import "./global.css"
 
-import { useColorScheme } from '@/components/useColorScheme';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
@@ -46,14 +33,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="welcome" options={{ headerShown: false }} />
       </Stack>
-    </ThemeProvider>
   );
 }
---
+++ app/welcome.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const WelcomePage = () => {
+  return (
+        <View>
+        <Text>welcomeoooooi</Text>
+        <Text>welcomeoooooi</Text>
+        <Text>welcomeoooooi</Text>
+        <StatusBar style="dark"/>
+        </View>
+  )
+}
+
+export default WelcomePage;
+
+const styles = StyleSheet.create({})
