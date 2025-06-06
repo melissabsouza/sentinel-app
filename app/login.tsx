7b9769d4cb46083229222925fb8bfc36e9d3f2b1
Melissa <melissabsouzaa@gmail.com>
2025-06-06 10:40:49 -0300

    fix: login redirect

--- app/login.tsx
+++ app/login.tsx
@@ -1,16 +1,18 @@
 import LoginForm from "../components/LoginForm";
 import { useRouter } from "expo-router";
-import {
-  SafeAreaView
-} from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Login = () => {
   const router = useRouter();
+  const replaceLoginScreen = () => {
+    router.dismissAll();
+    router.replace("/home");
+  };
 
   return (
-      <SafeAreaView style={{ flex: 1}}>
-        <LoginForm onSuccess={() => router.replace("/home")} />
-      </SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <LoginForm onSuccess={replaceLoginScreen} />
+    </SafeAreaView>
   );
 };
 
