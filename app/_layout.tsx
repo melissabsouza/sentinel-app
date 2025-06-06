0159451653e64662c7afa43d5956b5fd23e8ec34
Melissa <melissabsouzaa@gmail.com>
2025-06-06 10:41:49 -0300

    fix: ident

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,26 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
-import "./../global.css"
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '@/firebaseConfig';
-import AuthProvider from '@/components/AuthProvider';
-import { StatusBar } from 'expo-status-bar';
-
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
+import "./../global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/firebaseConfig";
+import AuthProvider from "@/components/AuthProvider";
+import { StatusBar } from "expo-status-bar";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-const [appIsReady, setAppIsReady] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, (user) => {
-    setAppIsReady(true);
-    SplashScreen.hide();
-  });
-  return () => unsubscribe();
-}, []);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setAppIsReady(true);
+      SplashScreen.hide();
+    });
+    return () => unsubscribe();
+  }, []);
 
   if (!appIsReady) {
     return null;
@@ -34,19 +30,18 @@ const [appIsReady, setAppIsReady] = useState(false);
 }
 
 function RootLayoutNav() {
-
   return (
     <AuthProvider>
-    <QueryClientProvider client={queryClient}>
-    <Stack>
-        <Stack.Screen name="welcome" options={{ headerShown: false }} />
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-        <Stack.Screen name="signin" options={{ headerShown: false }} />
-        <Stack.Screen name="home" options={{ headerShown: false }} />
-        <Stack.Screen name="edit/[id]" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="dark"/>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <Stack>
+          <Stack.Screen name="welcome" options={{ headerShown: false }} />
+          <Stack.Screen name="login" options={{ headerShown: false }} />
+          <Stack.Screen name="signin" options={{ headerShown: false }} />
+          <Stack.Screen name="home" options={{ headerShown: false }} />
+          <Stack.Screen name="edit/[id]" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style="dark" />
+      </QueryClientProvider>
     </AuthProvider>
   );
 }
