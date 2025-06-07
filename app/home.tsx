59a717f0e0afce8194913f5f3d18fe8ecb2558e1
Melissa <melissabsouzaa@gmail.com>
2025-06-06 22:24:45 -0300

    style: indent

--- app/home.tsx
+++ app/home.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Menu from '../components/Menu';
-import ShelterList from '../components/ShelterList';
-import {
-  SafeAreaView
-} from 'react-native-safe-area-context';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Menu from "../components/Menu";
+import ShelterList from "../components/ShelterList";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
@@ -25,21 +23,19 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   content: {
     flex: 1,
     paddingBottom: 70,
   },
   menu: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
     height: 70,
-    backgroundColor: '#fff',
-    borderTopWidth: 1,
-    borderColor: '#ccc',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });
