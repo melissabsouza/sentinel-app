2e432a6d4dbf9ef4f02e2f4164a8742611673400
Melissa <melissabsouzaa@gmail.com>
2025-06-06 22:37:11 -0300

    style: remove unused imports

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,16 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ProfileScreen from "@/components/ProfileScreen";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
-  return (
-    // <SafeAreaView style={{ flex: 1 }}>
-    <ProfileScreen />
-    // </SafeAreaView>
-  );
+  return <ProfileScreen />;
 };
 
 export default Profile;
-
-const styles = StyleSheet.create({});
--- components/ShelterForm.tsx
+++ components/ShelterForm.tsx
@@ -1,66 +1,82 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Text, ScrollView } from 'react-native';
-import { Shelter, Address, Contact, ShelterFormValues, ShelterCreateInput } from '../types/Shelter';
+import React, { useState } from "react";
+import {
+  TextInput,
+  Button,
+  StyleSheet,
+  Text,
+  ScrollView,
+} from "react-native";
+import { ShelterFormValues } from "../types/Shelter";
 
 type ShelterFormProps = {
   initialValues?: ShelterFormValues;
   onSubmit: (data: ShelterFormValues) => void;
 };
 
-const ShelterForm: React.FC<ShelterFormProps> = ({ initialValues, onSubmit }) => {
+const ShelterForm: React.FC<ShelterFormProps> = ({
+  initialValues,
+  onSubmit,
+}) => {
   const [form, setForm] = useState<ShelterFormValues>(
-  initialValues || {
-    name: '',
-    status: 'OPEN',
-    totalCapacity: 0,
-    currentCapacity: 0,
-    availableResources: '',
-    userEmail: 'user1@shelter.org',
-    address: {
-      street: '',
-      number: 0,
-      district: '',
-      city: '',
-      state: '',
-      cep: '',
-    },
-    contact: {
-      email: '',
-      phone: '',
-    },
-  } as ShelterFormValues
-);
+    initialValues ||
+      ({
+        name: "",
+        status: "OPEN",
+        totalCapacity: 0,
+        currentCapacity: 0,
+        availableResources: "",
+        userEmail: "user1@shelter.org",
+        address: {
+          street: "",
+          number: 0,
+          district: "",
+          city: "",
+          state: "",
+          cep: "",
+        },
+        contact: {
+          email: "",
+          phone: "",
+        },
+      } as ShelterFormValues)
+  );
 
   const handleChange = (field: string, value: any) => {
-  if (field.includes('.')) {
-    const [parent, child] = field.split('.');
-    setForm((prev) => {
-      const parentValue = (prev?.[parent as keyof ShelterFormValues] ?? {}) as Record<string, any>;
+    if (field.includes(".")) {
+      const [parent, child] = field.split(".");
+      setForm((prev) => {
+        const parentValue = (prev?.[
+          parent as keyof ShelterFormValues
+        ] ?? {}) as Record<string, any>;
 
-      return {
+        return {
+          ...prev,
+          [parent]: {
+            ...parentValue,
+            [child]:
+              child === "number" ||
+              child === "totalCapacity" ||
+              child === "currentCapacity"
+                ? Number(value)
+                : value,
+          },
+        };
+      });
+    } else {
+      setForm((prev) => ({
         ...prev,
-        [parent]: {
-          ...parentValue,
-          [child]:
-            child === 'number' || child === 'totalCapacity' || child === 'currentCapacity'
-              ? Number(value)
-              : value,
-        },
-      };
-    });
-  } else {
-    setForm((prev) => ({
-      ...prev,
-      [field]:
-        field === 'totalCapacity' || field === 'currentCapacity' ? Number(value) : value,
-    }));
-  }
-};
-
+        [field]:
+          field === "totalCapacity" ||
+          field === "currentCapacity"
+            ? Number(value)
+            : value,
+      }));
+    }
+  };
 
   const handleSubmit = () => {
     if (!form.name || !form.address || !form.contact) {
-      alert('Preencha todos os campos obrigatórios!');
+      alert("Preencha todos os campos obrigatórios!");
       return;
     }
     onSubmit(form);
@@ -69,14 +85,20 @@ const ShelterForm: React.FC<ShelterFormProps> = ({ initialValues, onSubmit }) =>
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text>Nome:</Text>
-      <TextInput style={styles.input} value={form.name} onChangeText={(text) => handleChange('name', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.name}
+        onChangeText={(text) => handleChange("name", text)}
+      />
 
       <Text>Capacidade Total:</Text>
       <TextInput
         style={styles.input}
         keyboardType="numeric"
         value={String(form.totalCapacity)}
-        onChangeText={(text) => handleChange('totalCapacity', text)}
+        onChangeText={(text) =>
+          handleChange("totalCapacity", text)
+        }
       />
 
       <Text>Capacidade Atual:</Text>
@@ -84,40 +106,92 @@ const ShelterForm: React.FC<ShelterFormProps> = ({ initialValues, onSubmit }) =>
         style={styles.input}
         keyboardType="numeric"
         value={String(form.currentCapacity)}
-        onChangeText={(text) => handleChange('currentCapacity', text)}
+        onChangeText={(text) =>
+          handleChange("currentCapacity", text)
+        }
       />
 
       <Text>Recursos:</Text>
-      <TextInput style={styles.input} value={form.availableResources} onChangeText={(text) => handleChange('availableResources', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.availableResources}
+        onChangeText={(text) =>
+          handleChange("availableResources", text)
+        }
+      />
 
       <Text>Email:</Text>
-      <TextInput style={styles.input} value={form.contact.email} onChangeText={(text) => handleChange('contact.email', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.contact.email}
+        onChangeText={(text) =>
+          handleChange("contact.email", text)
+        }
+      />
 
       <Text>Telefone:</Text>
-      <TextInput style={styles.input} value={form.contact.phone} onChangeText={(text) => handleChange('contact.phone', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.contact.phone}
+        onChangeText={(text) =>
+          handleChange("contact.phone", text)
+        }
+      />
 
       <Text>Rua:</Text>
-      <TextInput style={styles.input} value={form.address.street} onChangeText={(text) => handleChange('address.street', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.address.street}
+        onChangeText={(text) =>
+          handleChange("address.street", text)
+        }
+      />
 
       <Text>Número:</Text>
       <TextInput
         style={styles.input}
         keyboardType="numeric"
         value={String(form.address.number)}
-        onChangeText={(text) => handleChange('address.number', text)}
+        onChangeText={(text) =>
+          handleChange("address.number", text)
+        }
       />
 
       <Text>Bairro:</Text>
-      <TextInput style={styles.input} value={form.address.district} onChangeText={(text) => handleChange('address.district', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.address.district}
+        onChangeText={(text) =>
+          handleChange("address.district", text)
+        }
+      />
 
       <Text>Cidade:</Text>
-      <TextInput style={styles.input} value={form.address.city} onChangeText={(text) => handleChange('address.city', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.address.city}
+        onChangeText={(text) =>
+          handleChange("address.city", text)
+        }
+      />
 
       <Text>Estado:</Text>
-      <TextInput style={styles.input} value={form.address.state} onChangeText={(text) => handleChange('address.state', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.address.state}
+        onChangeText={(text) =>
+          handleChange("address.state", text)
+        }
+      />
 
       <Text>CEP:</Text>
-      <TextInput style={styles.input} value={form.address.cep} onChangeText={(text) => handleChange('address.cep', text)} />
+      <TextInput
+        style={styles.input}
+        value={form.address.cep}
+        onChangeText={(text) =>
+          handleChange("address.cep", text)
+        }
+      />
 
       <Button title="Salvar" onPress={handleSubmit} />
     </ScrollView>
@@ -131,7 +205,7 @@ const styles = StyleSheet.create({
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     padding: 8,
     borderRadius: 6,
   },
