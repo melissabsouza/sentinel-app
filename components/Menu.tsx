40cf7de5bf494eee3df033620e8d9f9962afe5bc
Melissa <melissabsouzaa@gmail.com>
2025-06-06 21:43:56 -0300

    fix: indent

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,42 +1,66 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { auth } from '@/firebaseConfig';
-import { useRouter } from 'expo-router';
-import { signOut } from 'firebase/auth';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { getAuth } from 'firebase/auth';
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { auth } from "@/firebaseConfig";
+import { useRouter } from "expo-router";
+import { signOut } from "firebase/auth";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { getAuth } from "firebase/auth";
 
 const firebaseAuth = getAuth();
 
 export default function Menu() {
-     const router = useRouter();
+  const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={() => router.push('/add')}>
-        <Ionicons name='add-circle-outline' color='#484C52' size={20}></Ionicons>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/add")}>
+        <Ionicons
+          name="add-circle-outline"
+          color="#484C52"
+          size={20}></Ionicons>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} onPress={() => router.push('/home')}>
-        <Ionicons name='home-outline' color='#484C52' size={20}></Ionicons>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/home")}>
+        <Ionicons
+          name="home-outline"
+          color="#484C52"
+          size={20}></Ionicons>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} onPress={() => router.push('/home')}>
-        <Ionicons name='person-outline' color='#484C52' size={20}></Ionicons>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/profile")}>
+        <Ionicons
+          name="person-outline"
+          color="#484C52"
+          size={20}></Ionicons>
       </TouchableOpacity>
 
-      <TouchableOpacity style={[styles.button, styles.logout]}
-      onPress={async () => {
-            try {
+      <TouchableOpacity
+        style={[styles.button, styles.logout]}
+        onPress={async () => {
+          try {
             console.log(firebaseAuth.currentUser);
             await signOut(auth);
-            router.replace('/welcome');
-            console.log('usuario deslogado:');
-            } catch (error) {
-            console.log('Erro ao deslogar:', error);
-            }
+            router.replace("/welcome");
+            console.log("usuario deslogado:");
+          } catch (error) {
+            console.log("Erro ao deslogar:", error);
+          }
         }}>
-        <Ionicons name='log-out-outline'  color='#484C52' size={20}></Ionicons>
+        <Ionicons
+          name="log-out-outline"
+          color="#484C52"
+          size={20}></Ionicons>
       </TouchableOpacity>
     </View>
   );
@@ -44,27 +68,26 @@ export default function Menu() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 10,
-    paddingLeft:15,
-    paddingRight:15,
+    paddingLeft: 15,
+    paddingRight: 15,
     paddingTop: 10,
     paddingBottom: 8,
     elevation: 4,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   button: {
     paddingVertical: 12,
     paddingHorizontal: 12,
     marginBottom: 10,
-    backgroundColor: '#eee',
+    backgroundColor: "#eee",
     borderRadius: 50,
   },
   logout: {
-    backgroundColor: '#ffdb58',
+    backgroundColor: "#ffdb58",
   },
   text: {
     fontSize: 16,
